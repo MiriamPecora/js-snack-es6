8d44309630ded9cff9651c1a5140884dff2462d7
MiriamPecora <miriampecora97@gmail.com>
2024-05-16 16:44:29 +0200

    [UPD] Completed first snack with the use of a for-each and if(). Then printed result on page with template literal

--- Snack1/main.js
+++ Snack1/main.js
@@ -2,27 +2,47 @@
 const racingBikes = [
     {
         name: "BMC Teammachine",
-        weigth: 6,
+        weight: 6,
     },
     {
         name: "Colnago C64",
-        weigth: 7,
+        weight: 7,
     },
     {
         name: "Specialized Roubaix Comp",
-        weigth: 8,
+        weight: 8,
     },
     {
         name: "Cannondale CAAD13 Disc 105",
-        weigth: 9,
+        weight: 9,
     },
     {
         name: "Lightweight Urgestalt",
-        weigth: 4,
+        weight: 4,
     },
     {
         name: "Factor O2 VAM",
-        weigth: 5,
+        weight: 5,
     },
 ];
 
+let lighterBikeKgs = racingBikes[0].weight;
+let lighterBikeName = racingBikes[0].name;
+const kg = "chilogrammi";
+const div = document.getElementById("print_bike");
+
+// Tramite for-each e istruzione condizionale if(), calcolare la bici col peso minore
+let heaviestBike = racingBikes.forEach((element) => {
+
+    if(element.weight < lighterBikeKgs) {
+        lighterBikeKgs = element.weight;
+        lighterBikeName = element.name;
+    };
+    
+});
+console.log(lighterBikeKgs);
+
+// Stampare a schermo la bici più leggera tramite template literal
+let h2 = document.createElement("h2");
+h2.textContent = `La bici più leggera è la ${lighterBikeName} con un peso di ${lighterBikeKgs} ${kg}`;
+div.append(h2);
